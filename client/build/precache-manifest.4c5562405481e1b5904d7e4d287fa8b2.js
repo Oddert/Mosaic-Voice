994aab4292ba432cf703175a396b340ddd3982d4
Robyn <oddert@gmail.com>
2020-01-22 14:55:48 +0000

    added build

---
+++ client/build/precache-manifest.4c5562405481e1b5904d7e4d287fa8b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49bf7d811df73d3825bf50184424a08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef0a3f21e5e3e48a67ce",
+    "url": "/static/css/main.201c3eb9.chunk.css"
+  },
+  {
+    "revision": "a1e9f29ac08b895878cd",
+    "url": "/static/js/2.27e690b7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.27e690b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef0a3f21e5e3e48a67ce",
+    "url": "/static/js/main.1e457238.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "47ab40478ebe90a99dd8ec24035a5eb8",
+    "url": "/static/media/goals.47ab4047.svg"
+  },
+  {
+    "revision": "53494d4e1c48fce8fad46bf77728f775",
+    "url": "/static/media/tools.53494d4e.svg"
+  },
+  {
+    "revision": "38d1ddcd141265085328f0474321d244",
+    "url": "/static/media/visualise.38d1ddcd.svg"
+  }
+]);
